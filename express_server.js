8dc49a364940d7c5f1581269743654d3ca0c8591
Jasmine <jasmine_singh1992@hotmail.com>
2020-02-18 02:06:21 +0000

    added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -4,10 +4,28 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for ( var i = 0; i < 6; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * 62));
+    }
+    return result;
+    
+ }
+ 
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,6 +52,11 @@ app.get("/urls/:shortURL", (req, res) => {
   let {shortURL} = req.params;
   let templateVars = { shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
 });
 
 app.listen(PORT, () => {

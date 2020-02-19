ab2761766c6155520650424de65200d73e452488
Jasmine <jasmine_singh1992@hotmail.com>
2020-02-19 03:53:41 +0000

    added logout

--- express_server.js
+++ express_server.js
@@ -3,9 +3,13 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
+const templateVars = {
+username: ""
+};
 
 // function to generate random string for short URL. Call it where necessary. 
 function generateRandomString() {
@@ -19,6 +23,7 @@ function generateRandomString() {
     
  }
  
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -43,17 +48,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars =  {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let {shortURL} = req.params;
-  let templateVars = { shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = { shortURL, longURL: urlDatabase[shortURL],  
+    username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -86,6 +93,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
+  res.redirect('/urls'); 
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username);
   res.redirect('/urls'); 
 });
 
